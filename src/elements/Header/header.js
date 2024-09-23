d2331843c70c727bb741e3b7d7acb95bb3f0da8e
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-09-23 08:48:51 +0200

    Update header.js

--- src/elements/Header/header.js
+++ src/elements/Header/header.js
@@ -12,16 +12,16 @@ const layout = () => {
     const divPage = document.createElement("header");
     divPage.className = "header__diversion";
 
-    // Logo clicable
+
     const logoHeader = document.createElement("a");
-    logoHeader.href = "/index.html"; // Redirige al home
+    logoHeader.href = "/index.html"; 
     const logoImg = document.createElement("img");
     logoImg.className = "header__logo";
     logoImg.src = "/assets/paulogo.svg";
     logoImg.alt = "Logo";
     logoHeader.appendChild(logoImg);
 
-    // Contenedor del menú
+
     const divMenuHeader = document.createElement("div");
     divMenuHeader.className = "icons__diversion";
 
@@ -31,35 +31,31 @@ const layout = () => {
         { className: "menu_about", href: "#", text: "About me" }
     ];
 
-    // Iteramos sobre los links y creamos los elementos de ancla
     links.forEach(link => {
         const menuLink = document.createElement("a");
         menuLink.className = link.className;
         menuLink.href = link.href;
         menuLink.textContent = link.text;
 
-        // Eventos para cargar contenido dinámico
         if (link.className === "menu_portfolio") {
             menuLink.addEventListener("click", (event) => {
-                event.preventDefault(); // Evitar redirección
-                LoadPortfolioContent();  // Llamamos a la función que carga el Portfolio
+                event.preventDefault();
+                LoadAboutMeContent();  
             });
         } else if (link.className === "menu_about") {
             menuLink.addEventListener("click", (event) => {
-                event.preventDefault(); // Evitar redirección
-                LoadAboutMeContent();  // Llamamos a la función que carga "Acerca de mí"
+                event.preventDefault(); 
+                LoadAboutMeContent();  
             });
         }
 
         divMenuHeader.appendChild(menuLink);
     });
 
-    // Añadimos el logo y el menú al contenedor del header
     divPage.append(logoHeader, divMenuHeader);
     page.appendChild(divPage);
 };
 
-// Exportamos el componente Header
 const Header = () => {
     return layout();
 };
